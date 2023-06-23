80a1e71fe3ee807838f80398ea0931d5be4608ef
Estani02 <estaniolmedo@gmail.com>
2023-06-23 10:02:14 -0300

    added app and its child routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Outlet } from "react-router-dom"
+import { SideBar } from "./views/SideBar"
+
 
 
 function App() {
 
   return (
-    <>
-      <div>Hello Word!</div>
-    </>
+    <div className="flex h-full w-full">
+      <SideBar />
+      <div className="flex justify-center ml-[237px] w-full">
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
